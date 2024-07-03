ecdeb413f1a9932e7ec51c9afff2a6bb1f1fd293
spricigom <emilispricigo.ifc@gmail.com>
2024-07-03 07:58:08 -0300

    v-model e button

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,21 @@
 import { reactive, ref } from "vue";
 
 const usuario = reactive({
-    nome: '';
+    nome: '',
+    email: '',
+    idade: 0,
+    senha: '',
+    nascimento: '',
 })
-
+const endereco = reactive({
+    CEP: '',
+    rua: '',
+    numero: '',
+    bairro: '',
+})
 const mostrarResultado = ref(false)
+
+//if (usuario.idade <= 18 && usuario.idade >= 60) {}
 
 </script>
 
@@ -16,18 +27,48 @@ const mostrarResultado = ref(false)
             <form @submit.prevent="mostrarResultado = !mostrarResultado">
                 <div class="row">
                     <label for="">Nome:</label>
-                    <input type="text" name="" id="">
+                    <input type="text" v-model="usuario.nome">
+                </div>
+                <div class="row">
+                    <label for="">Idade:</label>
+                    <input type="number" v-model="usuario.idade">
                 </div>
                 <div class="row">
                     <label for="">Email:</label>
-                    <input type="email" name="" id="">
+                    <input type="email" v-model="usuario.email">
                 </div>
                 <div class="row">
                     <label for="">Senha:</label>
-                    <input type="password" name="" id="">
+                    <input type="password" v-model="usuario.senha">
                 </div>
+                <div class="row">
+                    <label for="">Data de nascimento:</label>
+                    <input type="date" v-model="usuario.nascimento">
+                </div>
+                <div class="row">
+                    <label for="">CEP:</label>
+                    <input type="number" v-model="endereco.CEP">
+                </div>
+                <div class="row">
+                    <label for="">Rua:</label>
+                    <input type="text" v-model="endereco.rua">
+                </div>
+                <div class="row">
+                    <label for="">Número:</label>
+                    <input type="number" v-model="endereco.numero">
+                </div>
+                <div class="row">
+                    <label for="">Bairro:</label>
+                    <input type="text" v-model="endereco.bairro">
+                </div>
+                <button type="submit">Mostrar</button>
             </form>
         </div>
-        <button type="submit">Mostrar</button>
+    </div>
+    <div v-if="mostrarResultado">
+        <h2>Resultado</h2>
+        <p>Nome: {{ usuario.nome }}</p>
+        <p>Email: {{ usuario.email }}</p>
+        <p>Senha: {{ usuario.senha }}</p>
     </div>
 </template>
