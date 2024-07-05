fc5abb7dc582a84dc710a3e2a3c19f77282f03cf
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2024-07-05 15:20:24 -0300

    ajustes cards #2

--- src/App.vue
+++ src/App.vue
@@ -30,85 +30,192 @@ const mostrarResultado = ref(false)
 
 
 <template>
-<div class="fundo">
-
-    
 
 
-    <div class="container">
-        <h1>Projeto 1</h1>
-        <div class="formulario">
-            <form @submit.prevent="mostrarResultado = !mostrarResultado">
-                <div class="row" >
-                    <label for="">Nome:</label>
-                    <input type="text" v-model="usuario.nome">
-                </div>
-                <div class="row">
-                    <label for="">Idade:</label>
-                    <input type="number" v-model="usuario.idade">
-                </div>
-                <div class="row">
-                    <label for="">Email:</label>
-                    <input type="email" v-model="usuario.email">
-                </div>
-                <div class="row">
-                    <label for="">Senha:</label>
-                    <input type="password" v-model="usuario.senha">
-                </div>
-                <div class="row">
-                    <label for="">Data de nascimento:</label>
-                    <input type="date" v-model="usuario.nascimento">
-                </div>
-                <div class="row">
-                    <label for="">CEP:</label>
-                    <input type="number" v-model="endereco.CEP">
-                </div>
-                <div class="row">
-                    <label for="">Rua:</label>
-                    <input type="text" v-model="endereco.rua">
-                </div>
-                <div class="row">
-                    
-                    <label for="">Número:</label>
-                    <input type="number" v-model="endereco.numero">
-                </div>
-                <div class="row">
-                    <label for="">Bairro:</label>
-                    <input type="text" v-model="endereco.bairro">
-                </div>
-                <div class="row">
-                    <label for="">Estado:</label>
-                    <select name="estados" id="estados" v-model.lazy="usuario.estado">
-                        <option value="" disabled selected> Selecione um estado</option>
-                        <option v-for="(item, index) in usuario.estado" :key="index">  {{item}}</option>
-                    </select>
-                </div>
-                <div class="row">
-                    <label for="">Hobbies</label>
-                    <input type="text" v-model="diversos.hobbies">
-                </div>
-                <div class="row">
-                    <label for="">Linguagem de Programação</label>
-                    <input type="text" v-model="diversos.linguagem">
-                </div>
-                <div class="row">
-                    <label for="">Biografia</label>
-                    <input type="text" v-model="diversos.biografia">
-                </div>
-                <button type="submit" id="botao">Mostrar</button>
-            </form>
+
+
+    <div class="fundo">
+
+
+
+
+        <div class="container">
+            <h1>Projeto 1</h1>
+            <div class="formulario">
+                <form @submit.prevent="mostrarResultado = !mostrarResultado">
+
+
+
+
+
+
+
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Nome:</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="text" v-model="usuario.nome">
+                            </blockquote>
+                        </div>
+                    </div>
+
+
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Idade:</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="number" v-model="usuario.idade">
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Email:</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="email" v-model="usuario.email">
+                            </blockquote>
+                        </div>
+                    </div>
+
+
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Senha:</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="password" v-model="usuario.senha">
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Data de nascimento:</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="date" v-model="usuario.nascimento">
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">CEP:</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="number" v-model="endereco.CEP">
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Rua:</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="text" v-model="endereco.rua">
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Número:</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="number" v-model="endereco.numero">
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Bairro:</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="text" v-model="endereco.bairro">
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Estado:</label>
+                            <select name="estados" id="estados" v-model.lazy="usuario.estado">
+                                <option value="" disabled selected> Selecione um estado</option>
+                                <option v-for="(item, index) in usuario.estado" :key="index"> {{ item }}</option>
+                            </select>
+                        </div>
+                        <div class="card-body">
+
+                        </div>
+                    </div>
+
+                   
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Hobbies</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="text" v-model="diversos.hobbies">
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Linguagem de Programação</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="text" v-model="diversos.linguagem">
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <label for="">Biografia</label>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <input type="text" v-model="diversos.biografia">
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <button type="submit" id="botao">Mostrar</button>
+                </form>
+            </div>
         </div>
     </div>
-</div>
     <div v-if="mostrarResultado">
         <h2>Resultado</h2>
         <p>Nome: {{ usuario.nome }}</p>
         <p>Email: {{ usuario.email }}</p>
         <p>Senha: {{ usuario.senha }}</p>
-        <p>Data de nascimento: {{usuario.nascimento}}</p>
+        <p>Data de nascimento: {{ usuario.nascimento }}</p>
         <p>CEP: {{ endereco.CEP }}</p>
         <p>Rua: {{ endereco.rua }}</p>
         <p>Número: {{ endereco.numero }}</p>
-        <p>Bairro: {{endereco.bairro}}</p>
+        <p>Bairro: {{ endereco.bairro }}</p>
     </div>
 </template>
